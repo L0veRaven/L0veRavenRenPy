c10e280a1451af39331d05be730be5311feda0d0
YasmineRomero <l0ve.raven.stream@gmail.com>
2025-07-31 14:24:42 -0700

    Create practice.py

---
+++ Python Projects/Practice 1/practice.py
@@ -0,0 +1,7 @@
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+birth_year =  input("What is your birth year? ")
+age = 2025 - int(birth_year)
+new_patient = True
+
+print("Hello " + first_name + " " + int(age) + " " + new_patient)
